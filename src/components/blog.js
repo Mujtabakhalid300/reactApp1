f6f2a008fe0e15f15dc8d871c4f8ffd61778cf90
Mujtaba <mujtabakhalid20@gmail.com>
2022-01-06 23:46:26 +0500

    each post can be viewed withiout needing to open it from main app, added wait also

---
+++ src/components/blog.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Loader from "./loader";
+
+function Blog(props) {
+  const { id } = useParams();
+  const [state, setState] = useState([
+    "this is the title",
+    "this is the description",
+  ]);
+
+  useEffect(() => {
+    setState(props);
+  }, [props]);
+
+  if (state.props === undefined) {
+    return <Loader></Loader>;
+  } else if (state.props.length) {
+    return (
+      <div className="card">
+        {console.log(state.props)}
+        <div className="card-body">
+          <h5 className="card-title">{state.props[id - 1].id}</h5>
+          <p className="card-text">{state.props[id - 1].value}</p>
+          <Link to="/" className="btn btn-primary">
+            Go To Homepage
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    return <Loader></Loader>;
+  }
+
+  // return (
+  //   <div className="card">
+  //      {console.log(state.props)}
+  //      <div className="card-body">
+  //        <h5 className="card-title">hello</h5>
+  //        <p className="card-text">hello</p>
+  //        <Link to="/" className="btn btn-primary">
+  //          Go To Homepage
+  //        </Link>
+  //      </div>
+  //    </div>
+  //  );
+}
+
+export default Blog;
